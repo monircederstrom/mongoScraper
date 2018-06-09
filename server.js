151d9a269a9b43fa118bb7b9d31e839b9ab8aea3
Monir Cederstrom <32554823+monircederstrom@users.noreply.github.com>
2018-06-08 22:37:32 -0500

    updated server.js

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ var express = require("express");
 var mongojs = require("mongojs");
 var mongoose = require('mongoose');
 var logger = require("morgan");
+var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var axios = require("axios");
+var path = require("path");
+
 // Require request and cheerio. This makes the scraping possible
 var request = require("request");
 var cheerio = require("cheerio");
 
 //require all models
 var db = require("./models");
+
 
 var PORT = 3000;
 
@@ -21,104 +25,30 @@ var app = express();
 app.use(logger("dev"));
 
 //Use body-parser for form submissions
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //Use express.static to serve as  a static directory
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "/public")));
 
+//set views to handlebars
+app.engine("handlebars", exphbs({ 
+  extname: ".handlebars",
+  defaultLayout: "main",
+  layoutsDir: path.join(__dirname, '/views/layouts')
+}));
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, '/views'));
 //setting mongoose to use .then promises instead of default callbacks
 mongoose.Promise = Promise;
 
 //connect to mongoose
-mongoose.connect("mongodb://localhost/mongoScraper", {
-  useMongoClient: true
-});
+mongoose.connect("mongodb://localhost/mongoScraper").then(
+  () => { console.log("Mongoose is connected")},
+  err => { console.log(err) });
 //Routes
-
-//  route to scrape the bravo website
-app.get("/scrape", function(req, res) {
-  //grab body of html with request
-  axios.get("https://www.theonion.com/").then(function(response) {
-    // load into cheerio and save it to $ (shorthand selector) 
-    var $ = cheerio.load(response.data);
-    //grab every h2 with a headline class
-    $("article h1").each(function(i, element) {
-      //save to empy result object
-      var result = {}
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-      result.summary = $(this)
-        .children("p")
-        .text();
-         // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-      .then(function(dbArticle) {
-        // View the added result in the console
-        console.log(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        return res.json(err);
-    })
-  });
-  res.send("Scrape complete");
-});
-});
-
-// Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-  // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate("note")
-    .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Start the server
 app.listen(PORT, function() {
